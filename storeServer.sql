d421848151941eeb34b1733f43bac724c2bcd649
SadikRa <sadikrahman494@gmail.com>
2025-09-02 20:41:02 +0600

    insert data done

--- storeServer.sql
+++ storeServer.sql
@@ -1,19 +1,21 @@
- CREATE DATABASE storeServer;
+CREATE DATABASE storeServer;
 
- CREATE Table 'users' (
+
+\c storeServer;
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100) NOT NULL,
     email VARCHAR(200) UNIQUE NOT NULL,
-    
- )
+    password VARCHAR(255) NOT NULL,
+    isDeleted BOOLEAN DEFAULT FALSE,
+    createdAt TIMESTAMP DEFAULT NOW(),
+    updatedAt TIMESTAMP DEFAULT NOW()
+);
 
+INSERT INTO users (name, email, password)
+VALUES ('John Doe', 'john.doe@example.com', 'securepassword123');
 
---  CREATE TABLE users (
---     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---     name VARCHAR(100) NOT NULL,
---     email VARCHAR(150) UNIQUE NOT NULL,
---     password VARCHAR(255) NOT NULL,
---     isDeleted BOOLEAN DEFAULT FALSE,
---     createdAt TIMESTAMP DEFAULT NOW(),
---     updatedAt TIMESTAMP DEFAULT NOW()
--- );
+SELECT * FROM users;
