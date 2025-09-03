248b0a45ec1b63f057e64602da7b6ff5b853ba07
SadikRa <sadikrahman494@gmail.com>
2025-09-03 20:56:48 +0600

    product table added done

--- storeServer.sql
+++ storeServer.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE storeServer;
 
-
-\c storeServer;
+DROP DATABASE storeServer
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     name VARCHAR(100) NOT NULL,
     email VARCHAR(200) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -15,7 +14,22 @@ CREATE TABLE users (
     updatedAt TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO users (name, email, password)
-VALUES ('John Doe', 'john.doe@example.com', 'securepassword123');
+INSERT INTO
+    users (name, email, password)
+VALUES (
+        'sadik',
+        'sadik@gmail.com',
+        'sadik00'
+    )
 
 SELECT * FROM users;
+
+CREATE Table product (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid () name VARCHAR(400) NOT NULL,
+    quality VARCHAR(500) NOT NULL,
+    quantity INT NOT NULL DEFAULT (0),
+    isDeleted BOOLEAN DEFAULT FALSE,
+    createdAt TIMESTAMP DEFAULT NOW(),
+    updatedAt TIMESTAMP DEFAULT NOW()
+)
+
